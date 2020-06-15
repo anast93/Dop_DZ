6dd85bb4295b912807ca5e8599e34c202c61adc2
Anastasia Davydenko <latysheva93@list.ru>
2020-06-15 18:15:36 +0700

    dop07

---
+++ script_dop07.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье' ];
+
+let i;
+let str; 
+let date;
+let numDay;
+
+function infoWeek() {
+    date = new Date();
+    numDay = date.getDay();
+
+    for(i = 0; i < 5; i++) {
+        str = week[i];
+        if(i === (numDay-1)) {
+            console.log(`%c${str}`, "font-weight: bolder");
+        } else console.log(week[i]);
+    }
+
+    for(i = 6; i>4; i--) {
+        str = week[i];
+        console.log(`%c${str}`, "font-style: italic");
+    }
+}
+
+infoWeek();
+
+
+
+
